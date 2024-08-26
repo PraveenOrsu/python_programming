ff5e17ad164a2ef7ed57842d5e2d16db3a527815
PraveenOrsu <orsupraveenp@gmail.com>
2024-08-26 15:20:23 +0530

    Delete readfromkeyboard.py

--- readfromkeyboard.py
+++
@@ -1,6 +0,0 @@
-name=input("enter your name:")
-address=input("enter your address:")
-email=input("entr your email address:")
-phone_no=input("enter ypur phonne number:")
-
-print(f"name is{name}\n adddress is:{address}\n your email is:{email}\n your phone number is:{phone_no} ")
